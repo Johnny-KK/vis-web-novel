c6235b46569cba659af405ad5616e5244195684e
johnny <1249864985@qq.com>
2021-01-02 05:53:22 +0000

    新增目录页面

--- src/views/catalog/index.vue
+++ src/views/catalog/index.vue
@@ -1,11 +1,79 @@
 <template>
-  catalog
+  <ul class="catalog-list">
+    <li v-for="(item, i) in catalogList" :key="i">{{ item }}</li>
+  </ul>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, reactive } from 'vue';
 
 export default defineComponent({
-  name: 'vis-catalog'
+  name: 'vis-catalog',
+  setup() {
+    // 目录列表
+    const catalogList: string[] = reactive([]);
+    return { catalogList };
+  },
+  created() {
+    this.queryCatalogList();
+  },
+  methods: {
+    queryCatalogList() {
+      this.catalogList.push(
+        ...[
+          '第八百一十二章 栽赃嫁祸',
+          '第八百一十二章 栽赃嫁祸',
+          '第八百一十二章 栽赃嫁祸',
+          '第八百一十二章 栽赃嫁祸',
+          '第八百一十二章 栽赃嫁祸',
+          '第八百一十二章 栽赃嫁祸',
+          '第八百一十二章 栽赃嫁祸',
+          '第八百一十二章 栽赃嫁祸',
+          '第八百一十二章 栽赃嫁祸',
+          '第八百一十二章 栽赃嫁祸',
+          '第八百一十二章 栽赃嫁祸',
+          '第八百一十二章 栽赃嫁祸',
+          '第八百一十二章 栽赃嫁祸',
+          '第八百一十二章 栽赃嫁祸',
+          '第八百一十二章 栽赃嫁祸',
+          '第八百一十二章 栽赃嫁祸',
+          '第八百一十二章 栽赃嫁祸',
+          '第八百一十二章 栽赃嫁祸',
+          '第八百一十二章 栽赃嫁祸',
+          '第八百一十二章 栽赃嫁祸',
+          '第八百一十二章 栽赃嫁祸',
+          '第八百一十二章 栽赃嫁祸',
+          '第八百一十二章 栽赃嫁祸',
+          '第八百一十二章 栽赃嫁祸',
+          '第八百一十二章 栽赃嫁祸',
+          '第八百一十二章 栽赃嫁祸',
+          '第八百一十二章 栽赃嫁祸',
+          '第八百一十二章 栽赃嫁祸',
+          '第八百一十二章 栽赃嫁祸',
+          '第八百一十二章 栽赃嫁祸',
+          '第八百一十二章 栽赃嫁祸',
+          '第八百一十二章 栽赃嫁祸',
+          '第八百一十二章 栽赃嫁祸',
+          '第八百一十二章 栽赃嫁祸'
+        ]
+      );
+    }
+  }
 });
 </script>
+
+<style lang="scss" scoped>
+.catalog-list {
+  background-color: #e9faff;
+  padding: 1rem;
+  margin: 0;
+  height: 100%;
+  overflow-y: auto;
+
+  li {
+    list-style: none;
+    line-height: 2rem;
+    border-bottom: 1px dashed gainsboro;
+  }
+}
+</style>
